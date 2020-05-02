a15884ebb392fc5a5517e22fcc2832f2a2a1c346
yanagihara35319051 <bb35319051@ms.nagasaki-u.ac.jp>
2020-05-02 13:13:05 +0900

    確認したらコメントお願いします

--- k02/k02.c
+++ k02/k02.c
@@ -7,7 +7,9 @@ extern double p_stdnorm(double z);
 
 int main(void)
 {
-    double val;
+    double val_a,val_b;
+    double z_a, z_b, u_a=173.2, u_b=170.5, var_a=33, var_b=32;
+    double p_a, p_b , max_val=1, min_val=1;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
@@ -24,23 +26,32 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    while(fgets(buf,sizeof(buf),fp) != NULL){
-        sscanf(buf,"%lf",&val);
+    /*while(fgets(buf,sizeof(buf),fp) != NULL){ */
+      /*  sscanf(buf,"%lf",&val);  */
+    /*}  */
+    while(fscanf(fp, "%lf,%lf",&val_a,&val_b) !=EOF){
+      
+      z_a = (val_a - u_a)/sqrt(var_a);
+      z_b = (val_b - u_b)/sqrt(var_b);
 
-
-    
-
-
-
+      p_a = p_stdnorm(z_a);
+      p_b = p_stdnorm(z_b);
+      max_val = max_val * p_a;
+      min_val = min_val * p_b;
     }
+
+
+
+
+
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
 
-    printf("L_A: %f\n",max_val);
-    printf("L_B: %f\n",min_val);
+    printf("L_A: %.9f\n",max_val);
+    printf("L_B: %lf\n",min_val);
 
     return 0;
 
